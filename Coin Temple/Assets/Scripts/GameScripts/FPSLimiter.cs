9794ecaa4877f1a28fb93b4ae0c69c3142b91a72
DavidDEV <daviddev949@gmail.com>
2024-02-17 21:34:21 +0100

    Adding graphivs and functionalities.
    Adding the tilemap textures,  finding and solving bugs, programming better PlayerMovement, FPS Limiter to 60 and working on the AutoCoinMachine.
    
    -DavidDEV 17/02/2024 21:34.

---
+++ Coin Temple/Assets/Scripts/GameScripts/FPSLimiter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSLimiter : MonoBehaviour
+{
+
+    int FPSLimiterValue;
+
+    void Start()
+    {
+        FPSLimiterValue = 60;
+        Application.targetFrameRate = FPSLimiterValue;
+    }
+
+}
---
+++ Coin Temple/Assets/Scripts/GameScripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Transform tr;
+    Rigidbody2D rb2d;
+    public float SpeedX;
+    public float SpeedY;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        rb2d = GetComponent<Rigidbody2D>();
+        SpeedX = 10f;
+        SpeedY = 12000f;
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    public void Movement()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb2d.AddForce(new Vector2(0, SpeedY * Time.fixedDeltaTime));
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            tr.Translate(-SpeedX * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            tr.Translate(SpeedX * Time.deltaTime, 0, 0);
+        }
+    }
+
+}
