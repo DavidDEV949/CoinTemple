29c36272e869214e56c062de64f181079352f464
DavidDEV <daviddev949@gmail.com>
2024-03-02 16:32:34 +0100

    Working on the menu 1
    Adding transition from the Menu to the Game.
    Adding a LoadingBar.
    Fixed some PlayerMovement bugs.
    Working on the menu style.
    
    -DavidDEV 02/02/2024 16:32

---
+++ Coin Temple/Assets/Scripts/MenuScripts/ExitScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitScript : MonoBehaviour
+{
+   
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("On Application Quit");
+    }
+
+}
---
+++ Coin Temple/Assets/Scripts/MenuScripts/PlayScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayScript : MonoBehaviour
+{
+
+    public GameObject TransitionUp;
+    public GameObject TransitionDown;
+
+    public void Play()
+    {
+
+        //TransitionUp.transform.Translate(0, -350 * Time.deltaTime, 0);
+        //TransitionDown.transform.Translate(0, 350 * Time.deltaTime, 0);
+
+        TransitionUp.GetComponent<Rigidbody2D>().gravityScale = 0;
+        TransitionDown.GetComponent<Rigidbody2D>().gravityScale = 0;
+
+        TransitionUp.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -1);
+        TransitionDown.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 1);
+
+        //SceneManager.LoadScene("GameScene");
+
+    }
+
+
+}
